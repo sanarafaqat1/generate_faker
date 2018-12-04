c78d66bf73f4e297bff227691ae24270de21c916
Sana Rafaqat <sanarafaqat041@gmail.com>
2018-12-04 16:23:51 +0000

    V.1.0.0 Created a ReadMe

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# generate_faker
+# Generate Faker
+
+### Installing Repository
+To run this program, you first need to require a pre-defined gem called Faker. To do so, create a faker.rb file and at the put require 'faker'. To call a Http verb, get you first need to require HTTParty to the program. This gets the randomly generated word. We also require json, as we are getting the data from a json file.
+
+### Creating Methods
+To create a method, you first need to create a class and then start creating the different methods in the class. The first method I created was to get the full name from the random generator, I done this by calling Faker::Name.name.These are pre-defineed methods which you can get from https://github.com/stympy/faker .
+
+### To run the file
+To run this file, call ruby with the file name .rb and it should show the results of the generator. 
--- faker.rb
+++ faker.rb
@@ -1,27 +1,35 @@
 require 'faker'
+require 'httparty'
+require 'json'
 
 class Generator
 
-  attr_accessor :first_name
+  include HTTParty
+
+  base_uri 'api.postcodes.io'
+
+  def get_random_postcode
+    JSON.parse(self.class.get("/random/postcodes/").body)['result']['postcode']
+  end
 
   def full_name
-    @random_full_name = Faker::Name.name
+    Faker::Name.name
   end
 
   def address_random
-    @random_address = Faker::Address.city
+    Faker::Address.city
   end
 
   def zip_code
-    @random_zipcode = Faker::Address.zip_code
+    Faker::Address.zip_code
   end
 
   def job
-    @random_job = Faker::Job.title
+  Faker::Job.title
   end
 
   def nationality
-    @random_nationality = Faker::Nation.nationality
+    Faker::Nation.nationality
   end
 
   def birthday
@@ -31,7 +39,20 @@ class Generator
   def age
     Date.today.year - birthday.year
   end
-  
+
+  def phoneNumber
+    Faker::Config.locale = 'en-GB'
+    Faker::PhoneNumber.phone_number
+  end
+
+  def programming_language
+    Faker::ProgrammingLanguage.name
+  end
+
+  def company
+    Faker::Company.name
+  end
+
 end
 test = Generator.new
 puts test.full_name
@@ -40,3 +61,7 @@ puts test.zip_code
 puts test.job
 puts test.nationality
 puts test.age
+puts test.phoneNumber
+puts test.get_random_postcode
+puts test.programming_language
+puts test.company
